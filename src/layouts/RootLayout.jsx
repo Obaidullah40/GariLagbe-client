70ed2ccaeb64bb50051af975c7ba065b136ba8f7
Obaidullah40 <obaidullah2p17@gmail.com>
2025-06-26 23:59:38 +0600

    route and rootlayout complete

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import NavBar from '../pages/Shared/NavBar';
+import Footer from '../pages/Shared/Footer';
 
 const RootLayout = () => {
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className=''>
             <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import RootLayout from "../layouts/RootLayout";
+import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AddCar from "../pages/AddCar";
+import PrivateRoute from "../routes/PrivateRoute";
 
 
 const router = createBrowserRouter([
@@ -14,15 +19,21 @@ const router = createBrowserRouter([
                 Component: Home
             },
             {
-            //     path: 'register',
-            //     Component: Register
-            // },
-            // {
-            //     path: 'signIn',
-            //     Component: SignIn
+                path: "/register",
+                element: <Register />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/add-car",
+                element: <PrivateRoute><AddCar /></PrivateRoute>
             }
-        ]
+        ],
+        // errorElement:<NotFound/>
     },
+
 ]);
 
 
